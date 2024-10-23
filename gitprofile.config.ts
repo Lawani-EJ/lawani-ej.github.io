ea40588d8f61b7a6f34b157d54b05dfa9afb1ef9
Lawani Elyon John <104038966+Lawani-EJ@users.noreply.github.com>
2024-10-23 09:42:11 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,8 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['Lawani-EJ/GOMYCODE-Python-Projects-', 'Lawani-EJ/LearnlyAppTech'],
+                  ['Lawani-EJ/Hello-Python', 'Lawani-EJ/Apax'],// List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
